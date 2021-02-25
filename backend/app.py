97cae2fbd68c0bcf7dfaea4973c5e01427d59d55
Eric Shreve <ecshreve13@gmail.com>
2021-02-24 20:06:32 -0800

    backend: create sqlite db

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///jeppy.db"
+
+db = SQLAlchemy(app)
+class Clue(db.Model):
+    id = db.Column('id', db.Integer, primary_key = True)
+    game_id = db.Column(db.String(255))
+    clue_id = db.Column(db.String(255))
+    category = db.Column(db.String(255))
+    question = db.Column(db.String(255))
+    answer = db.Column(db.String(255))
+
+
+def __init__(self, game_id, clue_id, category, question, answer):
+        self.game_id = game_id
+        self.clue_id = clue_id
+        self.category = category
+        self.question = question
+        self.answer = answer
 
 @app.route('/')
 def index():
