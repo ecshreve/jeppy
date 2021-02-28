63f04a35d03adf69aebf55394f023c8f3e20c5e5
Eric Shreve <ecshreve13@gmail.com>
2021-02-28 00:37:25 -0800

    backend/test: update test file for new project structure

--- backend/tests/test_backend.py
+++ backend/tests/test_backend.py
@@ -1,5 +1,5 @@
-from .context import helpers
+from app.helpers import parse_raw_game_id
 
 
 def test_parse_raw_game_id():
-    assert helpers.parse_raw_game_id("Show #3966 - Monday, November 26, 2001") == ('3966', 'Monday, November 26, 2001')
+    assert parse_raw_game_id("Show #3966 - Monday, November 26, 2001") == ('3966', 'Monday, November 26, 2001')
