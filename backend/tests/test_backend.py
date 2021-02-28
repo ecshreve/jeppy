c4976e906a229452fc7a93bbf0b46930a1240574
Eric Shreve <ecshreve13@gmail.com>
2021-02-28 03:09:29 -0800

    backend/test: move test for unused helper

--- backend/tests/test_backend.py
+++
@@ -1,5 +0,0 @@
-from app.helpers import parse_raw_game_id
-
-
-def test_parse_raw_game_id():
-    assert parse_raw_game_id("Show #3966 - Monday, November 26, 2001") == ('3966', 'Monday, November 26, 2001')
