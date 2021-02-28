aa6f24f3cd7fb26e1c0915cd013c0d077f8da5a4
Eric Shreve <ecshreve13@gmail.com>
2021-02-28 03:09:29 -0800

    backend/test: move test for unused helper

--- backend/tests/test_helpers.py
+++ backend/tests/test_helpers.py
@@ -1,6 +1,9 @@
+'''Tests for the helpers module located in the app/ directory.'''
+
 import json
 
 from app.helpers import clean_game_entry
+from app.helpers import parse_raw_game_id
 
 
 def test_clean_game_entry(snapshot):
@@ -8,3 +11,8 @@ def test_clean_game_entry(snapshot):
         data = json.load(infile)
         clues = clean_game_entry(data[0])
         snapshot.assert_match(clues)
+
+
+def test_parse_raw_game_id():
+    assert parse_raw_game_id(
+        "Show #3966 - Monday, November 26, 2001") == ('3966', 'Monday, November 26, 2001')
