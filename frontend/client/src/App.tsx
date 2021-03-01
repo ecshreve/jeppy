83d2d597db7ff93dc6c78a9e88291b337c25ed28
Eric Shreve <ecshreve13@gmail.com>
2021-03-01 05:03:05 -0800

    frontend: clear local storage if this is a new build

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -4,7 +4,22 @@ import "./App.css";
 
 import Game from "../src/pages/Game";
 
+export const ENV_BUILD_TIME = process.env.REACT_APP_BUILD_TIME
+	? process.env.REACT_APP_BUILD_TIME
+	: "no-env-build-time";
+
 function App() {
+	// If the cachedBuildTime doesn't match the envBuildTime that indicates we
+	// are dealing with a fresh build of the App and should clear all local
+	// storage before proceeding. If they do match that indicates either a page
+	// reload, or the page has been closed and reopened.
+	const cachedBuildTime = localStorage.getItem("build_time");
+	if (ENV_BUILD_TIME !== cachedBuildTime) {
+		console.log("clearing");
+		localStorage.clear();
+		localStorage.setItem("build_time", ENV_BUILD_TIME);
+	}
+
 	return (
 		<div className="App">
 			<Game />
