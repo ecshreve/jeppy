b94bf49260d0b7520b669acd53657914033139d1
Eric Shreve <ecshreve13@gmail.com>
2021-03-01 01:53:56 -0800

    frontend: add status bar to game
    Add the statusbar component to the game. Handle selecting a new random
    game when the New Game button is pressed.

--- frontend/client/src/pages/Game.tsx
+++ frontend/client/src/pages/Game.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 
 import ClueComponent from "../components/clue/ClueComponent";
 import QuestionModal from "../components/question-modal/QuestionModal";
+import StatusBar from "../components/status-bar/StatusBar";
 
 import "./Game.css";
 
-import { Clue, getClues } from "../requests";
+import { Clue, getClues, getGameIds } from "../requests";
+import { DEVELOPMENT_GAME_ID } from "../consts";
 
 const getCategories = (clues: Clue[] | undefined): string[] => {
 	if (clues === undefined) {
@@ -64,10 +66,15 @@ export default function Game() {
 	const [data, setData] = useState<Clue[]>();
 	const [showQuestionModal, setShowQuestionModal] = useState(false);
 	const [selectedClue, setSelectedClue] = useState<Clue>();
+	const [allGameIds, setAllGameIds] = useState<string[]>([]);
+	const [currentGameId, setCurrentGameId] = useState(DEVELOPMENT_GAME_ID);
 
 	useEffect(() => {
-		getClues().then((result) => setData(result));
-	}, []);
+		getClues(currentGameId).then((result) => setData(result));
+		if (allGameIds.length <= 0) {
+			getGameIds().then((result) => setAllGameIds(result));
+		}
+	}, [currentGameId]);
 
 	const categories = getCategories(data);
 	const categoryToClueListMap = getCategoryToClueListMap(categories, data);
@@ -84,11 +91,16 @@ export default function Game() {
 		setShowQuestionModal(false);
 	};
 
+	const handleClickNewGame = () => {
+		const random = Math.floor(Math.random() * allGameIds.length);
+		setCurrentGameId(allGameIds[random]);
+	};
+
 	const renderCat = (catName: string, clues: Clue[]) => {
 		let myclues = clues.map((c, ind) => {
 			return (
 				<ClueComponent
-					key={ind}
+					key={currentGameId + ind}
 					value={200 * (ind + 1)}
 					clue={c}
 					handleSelect={handleClickClue}
@@ -111,13 +123,19 @@ export default function Game() {
 
 	return (
 		<>
-			<div className="flex-grid">
-				{renderCat(categories[0], categoryToClueListMap.get(categories[0])!)}
-				{renderCat(categories[1], categoryToClueListMap.get(categories[1])!)}
-				{renderCat(categories[2], categoryToClueListMap.get(categories[2])!)}
-				{renderCat(categories[3], categoryToClueListMap.get(categories[3])!)}
-				{renderCat(categories[4], categoryToClueListMap.get(categories[4])!)}
-				{renderCat(categories[5], categoryToClueListMap.get(categories[5])!)}
+			<div className="game-container">
+				<StatusBar
+					game_id={currentGameId}
+					handleClickNewGame={handleClickNewGame}
+				/>
+				<div className="flex-grid">
+					{renderCat(categories[0], categoryToClueListMap.get(categories[0])!)}
+					{renderCat(categories[1], categoryToClueListMap.get(categories[1])!)}
+					{renderCat(categories[2], categoryToClueListMap.get(categories[2])!)}
+					{renderCat(categories[3], categoryToClueListMap.get(categories[3])!)}
+					{renderCat(categories[4], categoryToClueListMap.get(categories[4])!)}
+					{renderCat(categories[5], categoryToClueListMap.get(categories[5])!)}
+				</div>
 			</div>
 			{showQuestionModal && (
 				<QuestionModal
