5d9a203aae2c2af2940b1f91f9e678dd5a029be5
Eric Shreve <ecshreve13@gmail.com>
2021-03-01 06:00:04 -0800

    frontend: hacky vertical centering for text

--- frontend/client/src/components/status-bar/StatusBar.tsx
+++ frontend/client/src/components/status-bar/StatusBar.tsx
@@ -13,10 +13,10 @@ export default function StatusBar(props: StatusBarProps) {
 	return (
 		<div className="status-bar">
 			<p>{props.game_id}</p>
-			<Button variant="warning" style={{marginRight: "5px"}} onClick={props.handleClickRestart}>
+			<Button style={{marginRight: "5px", background: "#031297", paddingBottom: ".2rem"}} onClick={props.handleClickRestart}>
 				Restart
 			</Button>
-			<Button variant="dark" onClick={props.handleClickNewGame}>
+			<Button style={{background: "#031297", paddingBottom: ".2rem"}} onClick={props.handleClickNewGame}>
 				New Game
 			</Button>
 		</div>
