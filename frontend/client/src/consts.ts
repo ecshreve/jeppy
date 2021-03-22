e62637a8e418d960a49ef13eb5b61c4e75085907
Eric Shreve <ecshreve13@gmail.com>
2021-03-21 23:51:07 -0700

    config: pull year and month from gameids

--- frontend/client/src/consts.ts
+++ frontend/client/src/consts.ts
@@ -12,3 +12,53 @@ export const DEVELOPMENT_GAME_ID = "Show #8236 - Monday, September 14, 2020";
 
 // MAX_TIMER_VAL represents the upper bound in seconds for the timer bar on questions.
 export const MAX_TIMER_VAL = 20;
+
+export const MONTHS = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+
+export const YEARS = [
+	"2021",
+	"2020",
+	"2019",
+	"2018",
+	"2017",
+	"2016",
+	"2015",
+	"2014",
+	"2013",
+	"2012",
+	"2011",
+	"2010",
+	"2009",
+	"2008",
+	"2007",
+	"2006",
+	"2005",
+	"2004",
+	"2003",
+	"2002",
+	"2001",
+	"2000",
+	"1999",
+	"1998",
+	"1997",
+	"1996",
+	"1995",
+	"1994",
+	"1993",
+	"1992",
+	"1991",
+	"1990",
+];
--- frontend/client/src/features/config/configSlice.ts
+++ frontend/client/src/features/config/configSlice.ts
@@ -1,15 +1,33 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { DEVELOPMENT_GAME_ID } from "../../consts";
+
+type ParsedGameId = {
+	raw: string;
+	year: string;
+	month: string;
+};
 interface ConfigState {
 	gameActive: boolean;
 	allGameIds: string[];
+	allParsedGameIds: ParsedGameId[];
 	currentGameId: string;
 }
 
 const initialState: ConfigState = {
 	gameActive: false,
 	allGameIds: [],
+	allParsedGameIds: [],
 	currentGameId: DEVELOPMENT_GAME_ID,
+};
+
+// "Show #3966 - Monday, November 26, 2001"
+const parseGameId = (gameId: string): ParsedGameId => {
+	const parts = gameId.split(",");
+
+	const y = parts[2].trim();
+	const m = parts[1].trim().trim().split(" ")[0];
+
+	return { raw: gameId, year: y, month: m };
 };
 
 export const configSlice = createSlice({
@@ -20,14 +38,19 @@ export const configSlice = createSlice({
 			state.gameActive = action.payload;
 		},
 		setAllGameIds: (state, action: PayloadAction<string[]>) => {
-			state.allGameIds = action.payload;
+			const gameIds = action.payload;
+			const parsedGameIds = gameIds.map((gid) => parseGameId(gid));
+			state.allGameIds = gameIds;
+			state.allParsedGameIds = parsedGameIds;
 		},
 		setCurrentGameId: (state, action) => {
 			state.currentGameId = action.payload;
+			console.log(parseGameId(state.currentGameId));
 		},
 		setRandomGameId: (state) => {
 			const random = Math.floor(Math.random() * state.allGameIds.length);
 			state.currentGameId = state.allGameIds[random];
+			console.log(parseGameId(state.currentGameId));
 		},
 	},
 });
